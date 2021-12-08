5ea799216fd3c1b5fe44942157c349c7b41540f2
artpros94 <prostakishyn.artem@icloud.com>
2021-12-08 18:13:22 +0200

    laracast lesson 13

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>Laracast lessons</title>
+</head>
+<body>
+
+    @foreach($posts as $post)
+
+    <article>
+  
+    <a href="/posts/{!! $post->slug  !!}"><h1>{!! $post->title  !!}</h1></a>
+    
+
+    <div>
+        {!! $post->excerpt  !!}
+    </div>
+    
+    </article>
+
+    @endforeach
+
+</body>
+</html>
